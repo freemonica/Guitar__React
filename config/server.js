c1b94eedd1e95cde06ee4c9f7cc9c50cc30c32d1
Monica Grant <monicagrantv@gmail.com>
2022-09-19 19:54:53 -0600

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '83da38fa7f3ed7be55ab65692f9551a9'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '5aaf99e6-4d6a-4c79-8804-e7583d9490c7'
+};
